3c2dd1d50a1394a602656e54f85fe8ad49d0796e
Mustafa Culha <merguven753@gmail.com>
2023-02-13 13:40:13 +0300

    1. Gun Sonu

--- src/test/java/day01_seleniumGiris/C02_driverMethotds.java
+++ src/test/java/day01_seleniumGiris/C02_driverMethotds.java
@@ -6,15 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class C02_driverMethotds {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
-        System.setProperty("Webdriver.chrome.driver","drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
 
         driver.get("https://www.wisequarter.com");
+        Thread.sleep(1000);
 
         driver.close();
 
