bcb2a7cd297b70ac5a5e6bf652195120bc5085af
Mustafa Culha <merguven753@gmail.com>
2023-02-13 13:24:07 +0300

    1. Gun Sonu

--- src/test/java/day01_seleniumGiris/C02_driverMethotds.java
+++ src/test/java/day01_seleniumGiris/C02_driverMethotds.java
@@ -1,12 +1,22 @@
 package day01_seleniumGiris;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C02_driverMethotds {
     public static void main(String[] args) {
 
-        git add README.md
-        git commit -m "first commit"
-        git branch -M main
-        git remote add origin https://github.com/culhamu/SeleniumTeam108.git
-        git push -u origin main
+
+        System.setProperty("Webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+        driver.get("https://www.wisequarter.com");
+
+        driver.close();
+
     }
 }
