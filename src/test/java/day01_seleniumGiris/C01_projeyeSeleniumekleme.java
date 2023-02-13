83eb2079da8b6eeb7cffb16e29ecb06bcd1f4a7c
Mustafa Culha <merguven753@gmail.com>
2023-02-13 13:16:26 +0300

    1. Gun Sonu

---
+++ src/test/java/day01_seleniumGiris/C01_projeyeSeleniumekleme.java
@@ -0,0 +1,19 @@
+package day01_seleniumGiris;
+
+public class C01_projeyeSeleniumekleme {
+    /*
+    Bir projede selenium ile otomasyon yapabilmek için projeye 2 şey eklemeliyiz
+    1-Selenium kütüphanesi
+    2- Kullanmak istediğimiz browser'larin selenium driver larını yüklemeliyiz
+    (Biz sadece chrome kullanıp sonra gelişmis framework'lere geçiş yapacağız)
+
+    Yeni açtığımızda üzerine gelerek sağ tuş yaptıktan sonra Open-In dedikten sonra
+    explorer diyoruz sonra selenium kütüphanesini ve chrome driver'ı Drivers'e ekliyoruz.
+
+    Daha sonra file'a tıklayıp projet structure seçip buradan modules dedikten sonra
+    dependincies'e tıklayıp + tuşuna basarak öncelikle selenium kütüphanesini ekleyip
+    ok dedikten sonra tekrar dan + basıp bu sefer lib altındaki kütüphaneyi ekleyip
+    önce apply yaptıktan sonra ok diyoruz.
+
+     */
+}
---
+++ src/test/java/day01_seleniumGiris/C02_driverMethotds.java
@@ -0,0 +1,12 @@
+package day01_seleniumGiris;
+
+public class C02_driverMethotds {
+    public static void main(String[] args) {
+
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/culhamu/SeleniumTeam108.git
+        git push -u origin main
+    }
+}
