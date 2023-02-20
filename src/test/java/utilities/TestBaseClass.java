3a0184073531648ba0af5849923cffd865fc9273
Mustafa Culha <merguven753@gmail.com>
2023-02-20 13:17:27 +0300

    5. Gun Sonu

--- src/test/java/utilities/TestBaseClass.java
+++ src/test/java/utilities/TestBaseClass.java
@@ -1,9 +1,7 @@
 package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
