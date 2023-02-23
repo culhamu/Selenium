072e4ebd03588848ffb04c945e9c27d13f701319
Mustafa Culha <merguven753@gmail.com>
2023-02-23 14:29:42 +0300

    8. Gun Sonu

---
+++ src/test/java/day08_waits_cookies_webTebles/C05_WebTables.java
@@ -0,0 +1,67 @@
+package day08_waits_cookies_webTebles;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class C05_WebTables extends TestBase {
+
+    @Test
+    public void test01(){
+
+        //1.“https://www.amazon.com” adresine gidin
+        driver.get("https://www.amazon.com");
+
+        //2.Sayfanin en altina inin
+        Actions actions=new Actions(driver);
+        actions.sendKeys(Keys.END).perform();
+
+        //3.Web table tum body’sini yazdirin
+        WebElement tumBody= driver.findElement(By.xpath("//tbody"));
+        System.out.println(tumBody.getText());
+
+        //4.Web table’daki satir sayisinin 10 oldugunu test edin
+        List<WebElement> satilarListesi=driver.findElements(By.xpath("//tbody/tr"));
+        Assert.assertEquals(10,satilarListesi.size());
+
+        //5.Tum satirlari yazdirin
+        int satirNo=1;
+        for (WebElement each:satilarListesi) {
+            System.out.println(satirNo+"-->"+each.getText());
+            satirNo++;
+        }
+
+        //6. Web table’daki sutun sayisinin 13 olduğunu test edin
+
+        List<WebElement> ucuncuSatirDataList=driver.findElements(By.xpath("//tbody/tr[3]/td"));
+        Assert.assertEquals(13,ucuncuSatirDataList.size());
+
+        //7. 5.sutunu yazdirin
+
+        List<WebElement> besinciSutunElementleri=driver.findElements(By.xpath("//tbody/tr/td[5]"));
+        satirNo=1;
+        for (WebElement each:besinciSutunElementleri) {
+
+            System.out.println(satirNo+"-->"+each.getText());
+            satirNo++;
+        }
+
+        //8.Satir ve sutun sayisini parametre olarak alip, hucredeki bilgiyi döndüren bir method olusturun
+
+        System.out.println(getData(3,5));
+    }
+    public  String getData(int satirNo,int sutunNo){
+
+        //3. satır 5. sütundaki bilgiyi yazdırın
+        String dataXPath="//tbody/tr["+satirNo+"]/td["+sutunNo+"]";
+
+        String istenenData=driver.findElement(By.xpath(dataXPath)).getText();
+        return istenenData;
+    }
+}
