caee64c427a4d02dbb08d8386af6a540cbff1aad
Mustafa Culha <merguven753@gmail.com>
2023-02-23 12:18:41 +0300

    8. Gun Sonu

---
+++ src/test/java/day08_waits_cookies_webTebles/C03_ExplicitlyWait.java
@@ -0,0 +1,49 @@
+package day08_waits_cookies_webTebles;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class C03_ExplicitlyWait {
+    @Test
+    public void test01(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        //4. Textbox’in etkin olmadigini(enabled) dogrulayın
+        WebElement textBox=driver.findElement(By.xpath("//input[@type='text']"));
+        Assert.assertFalse(textBox.isEnabled());
+
+        //5. Enable butonuna tıklayın ve textbox etkin oluncaya kadar bekleyin
+
+        WebElement enableButton= driver.findElement(By.xpath("(//button[@type='button'])[2]"));
+        enableButton.click();
+        //explicitlywait ile bekleyecek isek
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(40));
+        //wait objesi kullanarak bekleyelim
+        wait.until(ExpectedConditions.elementToBeClickable(textBox));
+
+        //6. “It’s enabled!” mesajinin goruntulendigini dogrulayın.
+        WebElement itsEnableElementi= driver.findElement(By.xpath("//p[@id='message']"));
+        Assert.assertTrue(itsEnableElementi.isDisplayed());
+
+        //7. Textbox’in etkin oldugunu(enabled) dogrulayın.
+        Assert.assertTrue(textBox.isEnabled());
+
+        driver.close();
+    }
+}
